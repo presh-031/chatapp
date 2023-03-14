b7d0937bdb1572ff6a7aa96082b9e63e0535177f
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-14 11:33:24 +0100

    fix: now hashing passwords

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,18 +1,28 @@
+import bcrypt from "bcrypt";
 import User from "../models/users.js";
 
 // REGISTER new user
 export const createNewUser = async (req, res) => {
   const { email, password, username, createdAt } = req.body;
-  // add document to db
+
+  // Add document to db
   try {
-    const user = await User.create({
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user document with the hashed password
+    const user = new User({
       email,
-      password,
+      password: hashedPassword,
       username,
     });
+
+    // Save the user document to the database
+    await user.save();
+
     res.status(201).json(user);
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       error: err.message,
     });
   }
