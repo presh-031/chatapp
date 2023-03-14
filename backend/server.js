0ec11d9687ac338dd46b77aa0cb7677ed459fb05
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-14 10:11:55 +0100

    feat: onto structuring users routes

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+// routes imports
+import usersRoutes from "./routes/users";
+
+const app = express();
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // Listen for requests
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log(`Connected to db & Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// routes
+app.use("/api/users", usersRoutes);
